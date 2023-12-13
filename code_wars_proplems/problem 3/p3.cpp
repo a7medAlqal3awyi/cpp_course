6d94ce168749d43979d960b6c0b3b2d2ca528a33
Ahmed <a7medalqalawyi@bmail.com>
2023-12-13 09:42:46 +0200

    finish problem 3

---
+++ code_wars_proplems/problem 3/p3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//https://www.codewars.com/kata/5545f109004975ea66000086/train/cpp
+isDivisible(int n, int x, int y)
+{
+    return n % x == 0 && n % y == 0 ? true : false;
+}
+int main()
+{
+}
