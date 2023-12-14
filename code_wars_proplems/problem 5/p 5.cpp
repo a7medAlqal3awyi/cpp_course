1964611891b2e8f5802d4d382234470d37e65a12
Ahmed <a7medalqalawyi@bmail.com>
2023-12-14 21:28:55 +0200

    finish problem 5

---
+++ code_wars_proplems/problem 5/p 5.cpp
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/cpp
+#include <iostream>
+#include <string>
+
+using namespace std;
+string reverseString(string str)
+{
+//     reserve(str.end(),str.begin() ); 
+    
+//     cout<< str;
+// return reserve(str.begin,str.end);
+
+}
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+int main()
+
+{
+    string name ="Ahmed ";
+    reverse(name.begin(),name.end()) ;
+    cout<<name; 
+
+  
+}
