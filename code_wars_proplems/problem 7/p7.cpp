aa413eb01486f5b2d9048f0e9a0edc14a73aafc3
Ahmed <a7medalqalawyi@bmail.com>
2023-12-15 22:25:30 +0200

    finish problem 7

---
+++ code_wars_proplems/problem 7/p7.cpp
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/57a77726bb9944d000000b06/train/cpp
+#include <iostream>
+
+using namespace std;
+int mango(int q, int p)
+{
+  
+    return q-q/3*p;
+}
+int main()
+{
+   q-q/3
+    cout << "Total =" << n*p;
+}
