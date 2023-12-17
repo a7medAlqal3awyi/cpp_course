9a1d5c800a33e8c2b173b47e4159ee4f106fdeaf
Ahmed <a7medalqalawyi@bmail.com>
2023-12-17 21:12:07 +0200

    finish problem 9

--- code_wars_proplems/problem 10/p10.cpp
+++ code_wars_proplems/problem 10/p10.cpp
@@ -1,6 +1,29 @@
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/train/cpp
 #include <iostream>
-using namespace std; 
-int main (){
-     
-   
+using namespace std;
+int main()
+{
+    string clean;
+    string h = "ahmed!!";
+    for (int i = 0; i < h.length(); i++)
+    {
+        if (h[i] == '!')
+        {
+            continue;
+        }
+        clean += h[i];
+    }
+    cout << clean << "\n";
+
+    // or 
+    string clean;
+    string h = "ahmed!!!!!!!";
+    for (int i = 0; i < h.length(); i++)
+    {
+        if (h[i] != '!')
+        {
+            clean += h[i];
+        }
+    }
+    cout << clean << "\n";
 }
