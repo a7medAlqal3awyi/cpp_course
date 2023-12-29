3d0c5e5c333f93757fd925c27819e5c9130b8d26
Ahmed <a7medAlqal3awyi>
2023-12-29 14:59:10 +0200

    Return Negative  Done

---
+++ code_wars_proplems/Return Negative/p.cpp
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/cpp
+int makeNegative(int num)
+{
+  if(num>0) return -1*num;
+  else if(num<0) return num ; 
+  else 
+  return 0;
+}
