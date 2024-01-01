0924e3799663ab03c25a7f3313e006284f0df2ed
Ahmed <a7medAlqal3awyi>
2024-01-01 21:10:06 +0200

    Is he gonna survive? Prolem done ✅

--- code_wars_proplems/Drink about/p.cpp
+++ code_wars_proplems/Drink about/p.cpp
@@ -1,7 +1,27 @@
 #include <string>
-
-std::string people_with_age_drink(int age) {
-  return age < 14 ? "drink toddy":
+ /*
+ يَسْأَلُونَكَ عَنِ الْخَمْرِ وَالْمَيْسِرِ ۖ قُلْ فِيهِمَا إِثْمٌ كَبِيرٌ وَمَنَافِعُ لِلنَّاسِ وَإِثْمُهُمَا أَكْبَرُ مِن نَّفْعِهِمَا ۗ  
+وَيَسْأَلُونَكَ مَاذَا يُنفِقُونَ قُلِ الْعَفْوَ ۗ كَذَٰلِكَ يُبَيِّنُ اللَّهُ لَكُمُ الْآيَاتِ لَعَلَّكُمْ تَتَفَكَّرُونَ"
+            219 سورة البقرة، الآية 
+      They ask you about wine and gambling. Say, "In them is great sin and [yet, some]
+      benefit for people. 
+      But their sin is greater than their benefit."
+      And they ask you what they should spend. Say, "The excess [beyond needs]." 
+      Thus Allah makes clear to you the verses [of revelation] that you might give thought.
+                                                        "Surah Al-Baqara Verse 219"
+*/
+std::string people_with_age_drdy":
          age < 18 ? "drink coke":
          age < 21 ? "drink beer": "drink whisky";
+}
+//  or
+ peoplef_with_age_drink(int age) {
+  if ( age < 14 )
+    return "drink toddy";
+  if ( age < 18)
+    return "drink coke";
+  if ( age < 21) 
+    return "drink beer";
+  return "drink whisky";
+  
 }
---
+++ code_wars_proplems/Is he gonna survive/p.cpp
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/cpp
+
+bool hero(int bullets, int dragons) {
+
+  if(bullets>=dragons*2)
+  return  true;
+  else 
+    return false ; 
+}
